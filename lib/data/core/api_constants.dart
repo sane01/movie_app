b820d43d234388458319fd49e81ac33d12f5995f
aaron <k.aaron.kim@gmail.com>
2021-06-30 18:38:16 +0900

    first commit

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class Apiconstants {
+  Apiconstants._();
+
+  static const String BASE_URL = 'https://api.themoviedb.org/3/';
+  static const String API_KEY = '1d61fb1f42c85b7646909518abfa4365';
+  static const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+}
